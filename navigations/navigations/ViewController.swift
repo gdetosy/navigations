6b8143e249fc42cb614de35b86e174517cdc50d2
gdetosy <tosyjdm@gmail.com>
2022-11-10 22:29:56 +0300

    go2root

--- navigations/navigations/ViewController.swift
+++ navigations/navigations/ViewController.swift
@@ -8,21 +8,18 @@
 import UIKit
 
 class ViewController: UIViewController {
- 
+    @IBAction func goToRoot(_ sender: Any) {
+        self.navigationController?.popToRootViewController(animated: true)
+    }
+
     @IBAction func toYellowView(_ sender: UIButton) {
-        
         let nextViewController = storyboardInstance.instantiateViewController(withIdentifier: "yellowViewController")
         self.navigationController?.pushViewController(nextViewController, animated: true)
-        
     }
 
     let storyboardInstance = UIStoryboard(name: "Main", bundle: nil)
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
-        
-       
-        
     }
-    
 }
