0f11d73fe8f0d6a3c901c1f3dc9de7699be9eadb
gdetosy <tosyjdm@gmail.com>
2022-11-12 21:55:41 +0300

    send text

---
+++ navigations/SecondViewController.swift
@@ -0,0 +1,38 @@
+//
+//  SecondViewController.swift
+//  navigations
+//
+//  Created by tosy on 12.11.22.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    @IBOutlet weak var dataTextField: UITextField!
+    var updatingData: String = ""
+    override func viewWillAppear(_ animated: Bool) { super.viewWillAppear(animated)
+        updateTextFieldData(withText: updatingData)
+    }
+        private func updateTextFieldData(withText text: String) {
+        dataTextField.text = text }
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+       
+    // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+
